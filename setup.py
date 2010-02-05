5ef14dd810f01a6bf680e3e5210b7e821e36a833
Justin Lilly <justin@justinlilly.com>
2010-02-05 00:43:04 -0500

    Adding setup.py bits.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+f = open('README.md')
+readme = f.read()
+f.close()
+
+setup(
+    name='django-gencal',
+    version='0.1',
+    description='django-gencal is a resuable Django application for rendering calendars.',
+    long_description=readme,
+    author='Justin Lilly',
+    author_email='justin@justinlilly.com',
+    url='http://code.justinlilly.com/django-gencal/',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    packages=['gencal'],
+    zip_safe=False,
+)
